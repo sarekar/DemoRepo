821bafa5a0732311611ff2fcb3fbab89f511df53
sarekar <sarekar@webuy.com>
2017-08-08 21:32:41 +0530

    testMessage() called code

--- FirstCode.cs
+++ FirstCode.cs
@@ -2,4 +2,11 @@ My First Code for git
 
 function Main(){
 	console.log('Main started');
+	// call testMessage()
+	testMessage();
+	
+
+	function testMessage() {
+		console.log('test succeded);
+	}
 }
